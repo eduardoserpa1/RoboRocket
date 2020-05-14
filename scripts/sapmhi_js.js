e7ee7cc23d7517e119b2e66f23c874f842ade92b
eduardoserpa1 <dudu.rserpa@gmail.com>
2020-05-13 23:45:39 -0300

    primeiros testes de comunicação com engine/html

---
+++ scripts/sapmhi_js.js
@@ -0,0 +1,19 @@
+function chamaPython(){
+
+    var {PythonShell} = require("python-shell")
+    var path = require("path")
+
+    var tipo_vela = 5
+
+    var opcoes = {
+        scriptPath : path.join(__dirname, '../engine/'),
+        args : [tipo_vela]
+    }
+
+    var sapmhi_py = new PythonShell('sapmhi_py.py', opcoes);
+
+
+    sapmhi_py.on('message', function(message){
+        swal(message);
+    })
+}
