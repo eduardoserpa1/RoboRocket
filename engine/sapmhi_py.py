57ae16044f0a2e8cca4b3b9cfc83b25c0968c20f
eduardoserpa1 <dudu.rserpa@gmail.com>
2020-05-23 18:21:52 -0300

    SAP-MHI funcionando

--- engine/sapmhi_py.py
+++ engine/sapmhi_py.py
@@ -1,12 +1,15 @@
 from iqoptionapi.stable_api import IQ_Option
 import time, json 
-import sys
+import sys, logging
 import iq_util
 
 div_final = ""
+lista_final = []
 
-tempo_vela = sys.argv[2]
-tipo_vela = sys.argv[1]
+tempo_vela = int(sys.argv[2])
+tipo_vela = int(sys.argv[1])
+
+
 
 c = iq_util.conexao_iq("dudu.rserpa@gmail.com","dudu123les")
 
@@ -18,30 +21,223 @@ while True:
         div_final += "conectado com sucesso !!!"
     break
 
+c.get_server_timestamp()
 
-dict_teste = c.get_all_open_time()
+lista_ativos = c.get_all_open_time()
+
+lista_digital = []
+
+
+
+for e in lista_ativos['digital'].keys(): 
+    if str(e) == "GBPJPY-OTC":
+        div_final += ""
+    else:
+        if lista_ativos['digital'][str(e)]['open'] == True:
+            lista_digital.append(str(e))
+
+
+horas_segundos = tempo_vela * 3600
+
+qtd_velas_horas = int(horas_segundos/tipo_vela)
+
+index_velas = int(qtd_velas_horas-1)
 
 hora_atual = time.time()
 
-vela = c.get_candles("USDJPY",3600,2,hora_atual)
+div_final += "<br><br><br>"
+div_final += "DIGITAL:"
+div_final += "<br><br>"
 
-lista_velas = dict(vela[0])
+lista_analise = []
 
-valida = lista_velas["open"] - lista_velas["close"]
+inicio= time.time()
 
-div_final += "<br>"+ str(lista_velas["open"])
-div_final += "<br>"+ str(lista_velas["close"])
+x= len(lista_digital)
+div_final += "<h1>Quantidade de ativos abertos no momento: "+str(x)+"</h1>"
 
-div_final += "<br>" + str(valida)
+for t in range(x):
 
-if valida > 0:
-    div_final += "<br> put"
-else:
-    div_final += "<br> call"
+    c.connect()
+
+    vela = c.get_candles(lista_digital[t],tipo_vela,qtd_velas_horas+1,hora_atual)
+    
+    lista_analise.append(lista_digital[t])
+
+    for i in range(qtd_velas_horas):
+
+        lista_velas = dict(vela[i])
+
+        valida = lista_velas["open"] - lista_velas["close"]
+
+        if valida > 0:
+         
+           lista_analise.append('p')
+        else:
+          
+           lista_analise.append('c')
+
+    hora_atual_analise = time.localtime()
+    hora_atual_analise = int(hora_atual_analise.tm_min)
+
+    qtd_elementos = len(lista_analise)
+
+    if hora_atual_analise % 5 == 0: #certo
+        lista_analise.pop(qtd_elementos-1)
+        lista_analise.pop(qtd_elementos-2)
+        
+    if hora_atual_analise % 5 == 1: #certo
+        lista_analise.pop(qtd_elementos-1)
+        lista_analise.pop(qtd_elementos-2)
+        lista_analise.pop(qtd_elementos-3)
+        
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+
+    if hora_atual_analise % 5 == 2: #certo
+        lista_analise.pop(qtd_elementos-1)
+        lista_analise.pop(qtd_elementos-2)
+        lista_analise.pop(qtd_elementos-3)
+        lista_analise.pop(qtd_elementos-4)
+        
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+
+    if hora_atual_analise % 5 == 3: #certo
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+    
+    if hora_atual_analise % 5 == 4: #certo
+        lista_analise.pop(qtd_elementos-1)
+        lista_analise.pop(1)
+    
+    #div_final += "<br><br>"+str(lista_analise)+"<br><br>"
+    qtd_entradas = int(len(lista_analise) / 5) 
+
+    qtd_p = 0
+    qtd_c = 0
+    qtd_mg = 0
+    lista_qtd_win = []
+
+    verifica_win = False
+    
+    for j in range(qtd_entradas):
+
+        verifica_win = False
+
+        for g in range(3):
+            if lista_analise[g+3] == 'p':
+                qtd_p += 1
+            else:
+                qtd_c += 1
+
+
+        if qtd_p > qtd_c:
+            if lista_analise[6] == 'c':
+                verifica_win = True
+                qtd_mg = 0
+            else:
+                if lista_analise[7] == 'c':
+                    verifica_win = True
+                    qtd_mg = 1
+                else:
+                    if lista_analise[8] == 'c':
+                        verifica_win = True
+                        qtd_mg = 2
+        
+
+        if qtd_c > qtd_p:
+            if lista_analise[6] == 'p':
+                verifica_win = True
+                qtd_mg = 0
+            else:
+                if lista_analise[7] == 'p':
+                    verifica_win = True
+                    qtd_mg = 1
+                else:
+                    if lista_analise[8] == 'p':
+                        verifica_win = True
+                        qtd_mg = 2
+
+        if verifica_win == True:
+            lista_qtd_win.append('w')
+            lista_qtd_win.append(qtd_mg)
+        else:
+            lista_qtd_win.append('l')
+            lista_qtd_win.append(2)
+        
+
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+        lista_analise.pop(1)
+        #; anti-gatinho
+
+    lista_aux = []
+    lista_aux.append(lista_analise[0])
+    lista_aux.append(lista_qtd_win)
+
+    lista_final.append(lista_aux)
+
+    lista_analise.clear()
+
+qtd_win = 0
+
+div_final += "<br><br>"
+
+for k in lista_final:
+    cont = 0
+    
+    for p in k:
+        cont_loss = 0
+        cont_win_0mg=0
+        cont_win_1mg=0
+        cont_win_2mg=0
+
+        if cont%2==0:
+            div_final += "<h1>"+str(p)+"</h1>"
+        else:  
+
+            cont_win = int(len(p) / 2)
+
+            for h in range(0,cont_win,2):
+                #div_final += str(p[h]) +"<br><br>"+str(h)+"<br>-<br>"
+                if p[h] == 'w':
+                    if p[h+1] == 0:
+                        
+                        cont_win_0mg += 1
+                        #cont_win_1mg += 1
+                        #cont_win_2mg += 1
+                    
+                    if p[h+1] == 1:
+                        cont_win_1mg += 1
+                        #cont_win_2mg += 1
+                    
+                    if p[h+1] == 2:
+                        cont_win_2mg += 1
+                else:
+                    cont_loss += 1
+
+                   
+            
+            total_entradas = cont_loss + cont_win_0mg + cont_win_1mg + cont_win_2mg
+            div_final += "<br><br> % DE ACERTO COM: <BR><BR>"
+            div_final += "0 MARTINGALE : " + str( round((cont_win_0mg*100) /total_entradas,1) ) + "%<br><br>"
+            div_final += "1 MARTINGALE : " + str( round(( (cont_win_0mg+cont_win_1mg) *100) /total_entradas,1) ) + "%<br><br>"
+            div_final += "2 MARTINGALE : " + str( round(( (cont_win_0mg+cont_win_1mg+cont_win_2mg) *100) /total_entradas,1) ) + "%<br><br>"
+           # div_final += "TOTAL : " + str( ((cont_win_0mg+cont_win_1mg+cont_win_2mg)*100)/total_entradas ) + "<br><br>"
+        
+        cont += 1
+
+
+fim = time.time()
+
+div_final += "<br><br><br>----  " + str(fim - inicio)
 
 print(div_final)
-#print(c.get_all_ACTIVES_OPCODE())
-#c.buy(1000,'USDJPY','PUT',5)
-#print(c.get_all_open_time())
 
-#print("tempo:"+tempo_vela+" - "+"tipo:"+tipo_vela)
+
